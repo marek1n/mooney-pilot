0182ccdfbacf5d36bfe22beadfe435c2f0ceb49c
marek1n <marek1nemecek@gmail.com>
2023-01-08 22:09:39 +0100

    initial

---
+++ utils/downloader.py
@@ -0,0 +1,71 @@
+import requests
+from os import getenv
+from pathlib import Path
+from tqdm import tqdm
+from functools import cache
+
+
+
+class AzureImgSearch:
+
+    def __init__(self):
+        self._KEY = getenv("SUBSCRIPTION_KEY")
+        self._SEARCH_URL = "https://api.bing.microsoft.com/v7.0/images/search"
+        self._HEADERS = {
+            "Ocp-Apim-Subscription-Key": self._KEY
+            }
+
+    @cache
+    def _image_urls(self, search_term: str, offset_max: int = 200) -> list:
+
+        params  = {
+            "q": search_term,
+            "license": "public", 
+            "imageType": "photo",
+            "aspect": "all"
+            }
+        
+        urls = []
+        offset = 0
+
+        print(f"\nSearching for '{search_term}' images...\n")
+
+        while (offset <= offset_max):
+            params["offset"] = offset
+            response = requests.get(self._SEARCH_URL, headers=self._HEADERS, params=params)
+            response.raise_for_status()
+            search_results = response.json()
+
+            for item in search_results["value"]:
+                url = item["contentUrl"]
+                urls.append(url)
+
+            # check if search not stuck on last page
+            if params["offset"] != search_results["nextOffset"]:
+                offset = search_results["nextOffset"]
+            else: 
+                break
+
+        print(len(urls), f"urls found for {search_term}\n")
+
+        return urls
+
+
+    def _save_images(url_list: list, search_term: str):
+        path = Path.cwd() / "final" / search_term
+        path.mkdir(parents=True, exist_ok=True)
+
+        print(f"\nSaving images to {path}")
+        print("\ndownloading images...\n")
+
+        for i, url in tqdm(enumerate(url_list), leave=False):
+            im_path = path / f"{search_term}_{i}.jpg"
+            try:
+                with open(im_path, 'wb') as f:
+                    img_data = requests.get(url, stream=True)
+                    f.write(img_data.content)
+            except Exception as e:
+                print(e)
+                continue
+
+        print("\nDownload finished")
---
+++ utils/process_images.py
@@ -0,0 +1,66 @@
+from PIL import Image
+from pathlib import Path
+from tqdm import tqdm
+
+from Katna.image import Image
+from Katna.writer import ImageCropDiskWriter
+
+# dls_path/term/cropped
+# dls_path/term/resized
+
+def crop_images(term: str, dls_path: (Path | str)):
+
+    cat_path = dls_path / term
+    imgs_cat = [imgpath for imgpath in cat_path.glob('**/*') 
+                            if ".DS_Store" not in str(imgpath)]
+
+    img_module = Image()
+
+    output_path = cat_path / "cropped"
+    output_path.mkdir(parents=True, exist_ok=True)
+
+    diskwriter = ImageCropDiskWriter(location=output_path)
+
+    print(f"Cropping {len(imgs_cat)} images ...\n")
+
+    for img in tqdm(imgs_cat):
+        try:
+            crop_list = img_module.crop_image_with_aspect(
+                file_path=str(img),
+                crop_aspect_ratio="1:1",
+                num_of_crops=1,
+                writer=diskwriter
+            )
+        except:
+            pass
+
+    print(f"Cropping finished.\nImages saved in {output_path}")
+
+
+def resize_images(term: str, dls_path: (Path | str)):
+
+    cat_path = dls_path / term
+    
+    img_module = Image()
+
+    cropped = cat_path / "cropped"
+    resized = cat_path / "resized"
+    resized.mkdir(parents=True, exist_ok=True)
+
+    resized_images = img_module.resize_image_from_dir(
+        dir_path=cropped,
+        target_width=500,
+        target_height=500,
+        down_sample_factor=8,
+    )
+
+    print("Resizing images ...\n")
+
+    for i, (filepath, resized_image) in enumerate(resized_images.items()):
+        img_module.save_image_to_disk(
+            resized_image, str(resized), f"{term}_{i}", ".jpeg"
+        )
+
+    print(f"Resizing finished.\nImages saved in {resized}")
+
+    
