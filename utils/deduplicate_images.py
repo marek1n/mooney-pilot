db2cc3b5000880b4190dd838e4bed981b79a2bf5
marek1n <marek1nemecek@gmail.com>
2023-04-23 20:49:23 +0200

    pHash dedup

--- utils/deduplicate_images.py
+++ utils/deduplicate_images.py
@@ -1,2 +1,39 @@
-# https://stackoverflow.com/questions/51688179/check-if-there-is-exactly-the-same-image-as-input-image
-raise NotImplementedError
+from pathlib import Path
+from PIL import Image, ImageOps
+import imagehash
+import numpy as np
+
+
+
+def compute_phash(image_path: str | Path) -> tuple[str, str, str]:
+    """Computes pHash for base image + vertically & horizontally flipped"""
+    # load greyscale image
+    image = Image.open(image_path)
+    
+    # mirror (vertical ax) + flip (horizontal ax)
+    mirror = ImageOps.mirror(image)
+    flip = ImageOps.flip(image)
+
+    phash, mphash, fphash = imagehash.phash(image), \
+                            imagehash.phash(mirror), \
+                            imagehash.phash(flip)
+    
+    # return tuple(map(str, (phash, mphash, fphash)))
+    return phash, mphash, fphash
+
+
+if __name__ == "__main__":
+
+    FOLDER = '/Users/mn/Desktop/g_images/stim_set_final_by_cat/garden tools'
+    files = [f for f in Path(FOLDER).glob("dis_*.jpg")]
+    filenames = list(map(lambda x: str(x).split('/')[-1], files))
+
+    phashz = [compute_phash(img) for img in files]
+    phash_list = phashz
+    # Compute Hamming distance between all pairs of tuples
+    for i, x in enumerate(phash_list):
+        for j, y in enumerate(phash_list):
+            if i < j:
+                hamming_ds = [(x[k] - y[l]) for k in range(3) for l in range(3)]
+                if any(d < 20 for d in hamming_ds):
+                    print(f"Hamming distance between {filenames[i]} and {filenames[j]}: {hamming_ds}")
